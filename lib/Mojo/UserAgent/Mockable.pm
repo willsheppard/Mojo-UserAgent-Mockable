e9e4dc1cb3b3a2055cd65699f3e68022ba39fb10
Johan Lindstrom <johan.lindstrom@broadbean.com>
2016-06-10 16:41:26 +0000

    compare_normalized_requests: POD

--- lib/Mojo/UserAgent/Mockable.pm
+++ lib/Mojo/UserAgent/Mockable.pm
@@ -70,7 +70,27 @@ playback mode. Specify 'all' to remove any headers from consideration. By defaul
 =attr ignore_body
 
 Ignore the request body entirely when comparing a request made with this class to a stored request 
-in playback mode. 
+in playback mode.
+
+=attr request_normalizer
+
+Optional subref. This is for when the requests require a more nuanced comparison (although it will
+be used in conjunction with the previous attributes).
+
+The subref takes two parameters: the current Mojo::Message::Request and the recorded one. The subref
+should modify these request objects in-place so that they match each other for the parts where your
+test doesn't care, e.g. set an id or timestamp to the same value in both requests.
+
+The return value is ignored, so a typical subref to ignore differences in any numerical id parts of
+the query path could look like this
+
+    request_normalizer => sub {
+        my ($req, $recorded_req) = @_;
+        for ($req, $recorded_req) {
+            $_->url->path( $_->url->path =~ s|/\d+\b|/123|gr );
+        }
+    },
+
 
 =head1 THEORY OF OPERATION
 
@@ -96,9 +116,16 @@ option).
 =head3 Request matching
 
 Two requests are considered to be equivalent if they have the same URL (order of query parameters
-notwithstanding), the same body content, and the same headers.  You may exclude headers from 
-consideration by means of the L</ignore_headers> attribute. You may excluse the request body from
-consideration by means of the L</ignore_body> attribute.
+notwithstanding), the same body content, and the same headers.
+
+Before comparing the current request with the recorded one, the requests are normalized using the
+subref in the request_normalizer attribute. The default is no normalization. See above for how to
+use it.
+
+You may also exclude headers from consideration by means of the L</ignore_headers> attribute. Or,
+you may excluse the request body from consideration by means of the L</ignore_body> attribute.
+
+
 
 =head1 CAVEATS
 
